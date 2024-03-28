330a7a97fcd703dbff6952a42eb3aa4149ad85b6
bernardoenock <bernardoenockgm@gmail.com>
2024-03-28 19:43:39 -0300

    feat: Create get_from_mongodb

--- database/mongodb.py
+++ database/mongodb.py
@@ -5,3 +5,10 @@ async def save_to_mongodb(data):
   db = client['similarweb']
   collection = db['website_data']
   await collection.insert_one(data)
+
+async def get_from_mongodb(url):
+    client = AsyncIOMotorClient('mongodb://localhost:27017')
+    db = client['similarweb']
+    collection = db['website_data']
+    result = await collection.find_one({'url': url})
+    return result
