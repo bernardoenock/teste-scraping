d547eae2e611b8a05a709de78220234167fc0161
bernardoenock <bernardoenockgm@gmail.com>
2024-04-01 13:22:34 -0300

    feat: Create and fix handlers

--- api/handlers.py
+++ api/handlers.py
@@ -1,7 +1,6 @@
 from aiohttp import web
-from bson import ObjectId
-from scraping.scraper import scrape_similarweb_data
-from database.mongodb import save_to_mongodb, get_from_mongodb
+from scraping.scraper import scrape_similarweb_data, scrape_similarweb_top_rank, scrape_similarweb_general_data_pro
+from database.mongodb import save_to_mongodb, get_from_mongodb, get_rank_from_mongodb
 
 async def save_info(request):
   data = await request.json()
@@ -10,6 +9,12 @@ async def save_info(request):
     return web.json_response({'error': 'URL not provided'}, status=400)
 
   scraped_data = await scrape_similarweb_data(url)
+  await save_to_mongodb(scraped_data)
+
+  return web.json_response({'id': str(scraped_data['_id'])}, status=201)
+
+async def save_rank(request):
+  scraped_data = await scrape_similarweb_top_rank()
   await save_to_mongodb(scraped_data)
 
   return web.json_response({'id': str(scraped_data['_id'])}, status=201)
@@ -25,8 +30,39 @@ async def get_info(request):
     if not result:
         return web.json_response({'error': 'Data not found'}, status=404)
     
-    if '_id' in result:
-        result['_id'] = str(result['_id'])
+    return web.json_response(result, status=200, content_type='application/json')
+
+async def get_info_rank(request):
+    result = await get_rank_from_mongodb()
     
-    return web.json_response(result, status=200)
+    if not result:
+        return web.json_response({'error': 'Data not found'}, status=404)
+    
+    return web.json_response(result, status=200, content_type='application/json')
+
+# SIMILARWEB PRO
+
+async def save_info_pro(request):
+  data = await request.json()
+  url = data.get('url')
+  if not url:
+    return web.json_response({'error': 'URL not provided'}, status=400)
+
+  scraped_data = await scrape_similarweb_general_data_pro(url)
+  await save_to_mongodb(scraped_data)
+
+  return web.json_response({'id': str(scraped_data['_id'])}, status=201)
+
+async def get_info_pro(request):
+    data = await request.json()
+    url = data.get('url')
+    if not url:
+        return web.json_response({'error': 'URL not provided'}, status=400)
+    
+    result = await get_from_mongodb(url)
+    
+    if not result:
+        return web.json_response({'error': 'Data not found'}, status=404)
+    
+    return web.json_response(result, status=200, content_type='application/json')
  
