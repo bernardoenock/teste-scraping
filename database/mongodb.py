7168c79783360e5f9ca7c146eaa835c5f1b9a512
bernardoenock <bernardoenockgm@gmail.com>
2024-04-01 13:23:35 -0300

    feat: Create get datas from mongodb

--- database/mongodb.py
+++ database/mongodb.py
@@ -10,5 +10,20 @@ async def get_from_mongodb(url):
     client = AsyncIOMotorClient('mongodb://localhost:27017')
     db = client['similarweb']
     collection = db['website_data']
-    result = await collection.find_one({'url': url})
-    return result
+    results = []
+    async for document in collection.find({'meta.request.domain': url}):
+        document['_id'] = str(document['_id'])
+        results.append(document)
+    
+    return results
+
+async def get_rank_from_mongodb():
+    client = AsyncIOMotorClient('mongodb://localhost:27017')
+    db = client['similarweb']
+    collection = db['website_data']
+    results = []
+    async for document in collection.find({'top_sites.rank': 1}):
+        document['_id'] = str(document['_id'])
+        results.append(document)
+    
+    return results
