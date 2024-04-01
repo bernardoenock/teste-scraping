b638ad7f5c11269a0e4e4b68910b4fcd563ad93a
bernardoenock <bernardoenockgm@gmail.com>
2024-04-01 13:22:52 -0300

    feat: Create new routes

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,10 @@
 from aiohttp import web
-from .handlers import save_info, get_info
+from .handlers import save_info, get_info, save_rank, get_info_rank, save_info_pro, get_info_pro
 
 def setup_routes(app):
   app.router.add_post('/save_info', save_info)
-  app.router.add_post('/get_info', get_info)
+  app.router.add_get('/get_info', get_info)
+  app.router.add_post('/save_rank', save_rank)
+  app.router.add_get('/get_info_rank', get_info_rank)
+  app.router.add_post('/ save_info_pro',  save_info_pro)
+  app.router.add_get('/get_info_pro', get_info_pro)
